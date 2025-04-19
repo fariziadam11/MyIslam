592508814007267ea4b37464826ed298627bde4a
unknown <fariziadam508@gmail.com>
2025-04-19 18:28:13 +0700

    add icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { Landmark } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
 
 const Header = () => {
@@ -6,7 +5,11 @@ const Header = () => {
     <header className="sticky top-0 z-10 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm shadow-sm">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center space-x-2">
-        <Landmark className="h-6 w-6 text-emerald-600 dark:text-emerald-400" />
+          <img
+            src="/emerald.png"
+            alt="Masjid Icon"
+            className="h-6 w-6"
+          />
           <h1 className="text-xl font-bold text-gray-800 dark:text-white">
             Jadwal Sholat
           </h1>
