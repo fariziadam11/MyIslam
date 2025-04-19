e50022df022f81ab4f21247b6ce4b2c948166faf
unknown <fariziadam508@gmail.com>
2025-04-19 18:24:38 +0700

    add icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Fuel as Mosque } from 'lucide-react';
+import { Landmark } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
 
 const Header = () => {
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="sticky top-0 z-10 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm shadow-sm">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center space-x-2">
-          <Mosque className="h-6 w-6 text-emerald-600 dark:text-emerald-400" />
+        <Landmark className="h-6 w-6 text-emerald-600 dark:text-emerald-400" />
           <h1 className="text-xl font-bold text-gray-800 dark:text-white">
             Jadwal Sholat
           </h1>
